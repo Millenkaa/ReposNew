1fdacf974a6116818a73936a77e8eb438e25396c
Milena.Gataullina@urfu.me <Milena.Gataullina@urfu.me>
2024-10-07 20:43:06 +0500

    Задание 1 выполнено

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Парус (Белеет парус одинокий)");
+
+                Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Белеет парус одинокой");
+            Console.WriteLine("В тумане моря голубом!..");
+            Console.WriteLine("Что ищет он в стране далекой?");
+            Console.WriteLine("Что кинул он в краю родном?..");
+            Console.WriteLine("Играют волны — ветер свищет,");
+            Console.WriteLine("И мачта гнется и скрыпит…");
+            Console.WriteLine("Увы! он счастия не ищет");
+            Console.WriteLine("И не от счастия бежит!");
+            Console.WriteLine("Под ним струя светлей лазури,");
+            Console.WriteLine("Над ним луч солнца золотой…");
+            Console.WriteLine("А он, мятежный, просит бури,");
+            Console.WriteLine("Как будто в бурях есть покой!");
+            
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("М.Ю.Лермонтов");
+
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите действительное число");
+            var x = double.Parse(Console.ReadLine());
+            var y = MyFunction(x);
+            Console.WriteLine($"+(x)={y}");
+
+            Console.ReadKey();
+               
+        }
+        static double MyFunction(double x)
+
+        {
+           //throw new NotImplementedException();
+           return Math.S
+        }
+
+    }
+}
