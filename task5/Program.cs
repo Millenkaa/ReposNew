88341f9bcda4eedbdcf834aca6ba5d6fac59f5fd
Milena.Gataullina@urfu.me <Milena.Gataullina@urfu.me>
2024-10-14 17:20:08 +0500

    Задание 6.1

---
+++ task5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double y = F(2, 3) + F(7, 8) + F(1, 4);
+            Console.WriteLine("x = " + Math.Round(y, 3));
+
+            Console.ReadKey();
+        }
+        static double F(double a, double b)
+        {
+            return Math.Sqrt(a + Math.Tan(b) / Math.Pow(a, 2)) + Math.Pow(b, 2);
+        }
+    }
+}
+    
---
+++ task6.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "программа";
+            Console.WriteLine($"Из слова \"{$}\" получили ");
+
+            var word1 = s
+                .Remove()
+                .Remove()
+
+
+                Console.WriteLine(word1);
+
+            var word2 = 
+                s.
+                .
+
+            Console.ReadKey();
+
+        }
+    }
+}
