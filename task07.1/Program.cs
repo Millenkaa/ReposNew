b0ec870aef40240a4e4bbb26b05a8495ab873f0e
Milena.Gataullina@urfu.me <Milena.Gataullina@urfu.me>
2024-10-21 15:53:17 +0500

    задание 7.1

---
+++ task07.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = GetNumber("m");
+            var n = GetNumber("n");
+            var k = GetNumber("k");
+
+            if (IsStatementTrue(m, n, k))
+                Console.WriteLine("Утверждение истинно");
+            else
+                Console.WriteLine("Утверждение ложно");
+
+            Console.ReadKey();
+
+        }
+
+        static bool IsStatementTrue(int m, int n, int k)
+        {
+            return m < 0 && n < 0 && k < 0;
+
+        }
+
+        static int GetNumber(string numberName)
+        {
+            Console.WriteLine($"Введите число {numberName}");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
