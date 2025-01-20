0f62434e094dedcc3f37fbe7ab04701a4dcdaaa8
Milena.Gataullina@urfu.me <Milena.Gataullina@urfu.me>
2025-01-20 13:14:18 +0500

    выполнено

---
+++ taskk10.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskk10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double p0 = 1.29;
+            const double C = 1.25e-4;
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введите значение n (10 ≤ n ≤ 100): ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 10 || n > 100)
+            {
+                Console.WriteLine("Число n должно быть в пределах от 10 до 100.");
+                return;
+            }
+
+            Console.WriteLine("Таблица зависимости плотности воздуха от высоты:");
+            Console.WriteLine("Высота (м)\tПлотность воздуха (кг/м³)");
+
+            for (int i = 0; i <= n * 100; i += 100)
+            {
+                double height = i; // Высота в метрах 
+                double density = p0 * Math.Exp(-C * height);
+                Console.WriteLine($"{height}\t\t{density:F3}");
+            }
+        }
+    }
+}
+    
---
+++ taskk10.6/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskk10._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите значение a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение b (должно быть больше a): ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение k (2 ≤ k ≤ 10): ");
+            int k = int.Parse(Console.ReadLine());
+
+            if (a >= b(k > 10))
+            {
+                Console.WriteLine("Ошибка ввода данных. Убедитесь, что a < b и 2 ≤ k ≤ 10.");
+                return;
+            }
+
+            Console.WriteLine($"Целые числа из промежутка от {a} до {b}, у которых сумма делителей кратна {k}:");
+
+            for (int i = a; i <= b; i++)
+            {
+                int sumOfDivisors = CalculateSumOfDivisors(i);
+
+                if (sumOfDivisors % k == 0)
+                {
+                    Console.WriteLine($"{i} (сумма делителей: {sumOfDivisors})");
+                }
+            }
+        }
+
+        static int CalculateSumOfDivisors(int number)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+
+            sum += number;
+
+            return sum;
+        }
+    }
+}
