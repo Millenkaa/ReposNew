53f09d742d7ec6b10ce0051ce985bdef8ca679e1
Milena.Gataullina@urfu.me <Milena.Gataullina@urfu.me>
2025-01-22 12:57:51 +0500

    Выполнено

---
+++ study/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = Console.ReadLine ();
+            Console.WriteLine($"Hello {name}" );
+
+            Console.ReadKey();
+
+        }
+    }
+}
